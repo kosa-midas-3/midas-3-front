c273b0f18e4120f5aaa005bbf40d317dcc363de7
limjh5699 <limjh5699@gmail.com>
2022-11-03 12:59:37 +0900

    로그인 제작

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <Layout
       justify={localStorage.getItem("username") ? "space-between" : "center"}
     >
-      asd
+      <Login />
     </Layout>
   );
 }
